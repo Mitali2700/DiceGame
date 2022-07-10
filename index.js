5f171d4aa7b2a09ebe40c710c0b16170040df377
Mitali2700 <201B155@juetguna.in>
2022-07-10 16:16:38 +0530

    dice 1 is working

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 var randomnumber1= Math.floor(Math.random()*6) + 1;
+
+var randomDiceImage = "dice"+randomnumber1+".png";  //dice1.png ti dice6.png
+
+var randomImageSource ="images/"+ randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+
+
