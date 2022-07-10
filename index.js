e7f342c4bdde7aad1732c53ce130aebdcd50a79a
Mitali2700 <201B155@juetguna.in>
2022-07-10 16:35:48 +0530

    Game give o/p as Win, Loose or Draw

--- index.js
+++ index.js
@@ -17,5 +17,15 @@ var image2 = document.querySelectorAll("img")[1];
 
 document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2)
 
+if( randomnumber1 > randomnumber2)
+{
+    document.querySelector("h1").innerHTML ="Player 1 Wins🎌";
+}else if(randomnumber1 < randomnumber2)
+{
+    document.querySelector("h1").innerHTML ="Player 2 Wins🎌";
+}else{
+    document.querySelector("h1").innerHTML ="Draw!";
+}
+
 
 
