fc9d5b595fb494d28e2607bf2cced312af6dfa6c
Mitali2700 <201B155@juetguna.in>
2022-07-10 16:24:22 +0530

    dice 2 is also working

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ var randomnumber1= Math.floor(Math.random()*6) + 1;
 
 var randomDiceImage = "dice"+randomnumber1+".png";  //dice1.png ti dice6.png
 
-var randomImageSource ="images/"+ randomDiceImage;
+var randomImageSource1 ="images/"+ randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src", randomImageSource);
+image1.setAttribute("src", randomImageSource1);
 
+
+var randomnumber2 = Math.floor(Math.random()*6) + 1;
+
+var randomImageSource2 ="images/dice"+ randomnumber2+".png";
+
+var image2 = document.querySelectorAll("img")[1];
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2)
 
 
 
